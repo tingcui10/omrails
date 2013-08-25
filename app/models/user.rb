8d64c8a81f8219b5bc726cdfc43fad71fa6ae911
Ting Cui <ting.cui10@gmail.com>
2013-08-24 22:41:50 -0400

    Add Pins Scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, #:recoverable, 
          :rememberable, :trackable, :validatable
 
+
+  validates_presence_of :name
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+
   # attr_accessible :title, :body
 
   has_many :pins, :dependent => :destroy
